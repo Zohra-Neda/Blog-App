13f0cd3e3498bbbe7760bc0866f40a3eb9431b3f
zohra-neda <zohra.neda@gmail.com>
2023-09-20 17:19:23 +0430

    Define methods for all created model classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
- belongs_to :author, class_name: 'User'
- belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
- def update_post_comments_counter
+  def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
- end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
- belongs_to :author, class_name: 'User', foreign_key: :author_id
- has_many :comments
- has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
- after_create :update_user_posts_counter
+  after_create :update_user_posts_counter
 
- def update_user_posts_counter
+  def update_user_posts_counter
     author.update(posts_counter: author.post.count)
- end
+  end
 
- def recent_comments(limit = 5)
+  def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
- end
+  end
 end
