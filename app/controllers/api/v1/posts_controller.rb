b0603a20264977ce4f8bcf1168a54529dbe9e2ea
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-10-05 13:22:06 +0300

    Add api for post controller.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::PostsController < ApplicationController
-end
+    load_and_authorize_resource
+    def index
+      @posts = Post.all.order('created_at')
+      render json: { success: true, data: { posts: @posts } }, status: :ok
+    end
+  end
